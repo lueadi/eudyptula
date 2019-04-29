516d418cd0ceb98023df3a5e75060d0d95204233
lueadi <397296981@qq.com>
2019-04-29 16:18:30 +0800

    fix s32v env & make file

---
+++ task1/export.sh
@@ -0,0 +1,7 @@
+# Toolchain path for bootloader & v234ce_m4_standalone & m4 folders
+#export CROSS_COMPILE=/home/xiaomin/GCC_6.3.1/NXP/gcc-6.3-arm32-eabi/bin
+#Toolchain path for linux_app & linux_kernel folders
+export CROSS_COMPILE=/home/nick/toolchain/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+export UBOOT_SRC=/home/nick/u-boot
+export LINUX_SRC=/home/nick/linux
+export  ARCH=arm64
